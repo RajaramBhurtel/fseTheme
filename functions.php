1a99d981a96e3e4886a6aefc43ee12165814eda7
Sabin2 <sabinbhurtel@gmail.com>
2023-01-03 11:48:21 +0545

    Learning FSE

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
-if ( ! function_exists( 'emptytheme_support' ) ) :
-	function emptytheme_support()  {
+if ( ! function_exists( 'fsetheme_support' ) ) :
+	function fsetheme_support()  {
 
 		// Adding support for core block visual styles.
 		add_theme_support( 'wp-block-styles' );
@@ -9,15 +9,15 @@ if ( ! function_exists( 'emptytheme_support' ) ) :
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
 	}
-	add_action( 'after_setup_theme', 'emptytheme_support' );
+	add_action( 'after_setup_theme', 'fsetheme_support' );
 endif;
 
 /**
  * Enqueue scripts and styles.
  */
-function emptytheme_scripts() {
+function fsetheme_scripts() {
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'emptytheme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'fsetheme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'emptytheme_scripts' );
+add_action( 'wp_enqueue_scripts', 'fsetheme_scripts' );
