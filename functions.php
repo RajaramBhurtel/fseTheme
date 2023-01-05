408b841efd08bb7e80398f23902741a0d2e16e7f
Sabin2 <sabinbhurtel@gmail.com>
2023-01-05 16:45:37 +0545

    banner added

--- functions.php
+++ functions.php
@@ -21,3 +21,42 @@ function fsetheme_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'fsetheme_scripts' );
+
+if ( ! function_exists( 'evit_fse_fonts_url' ) ) :
+	/**
+	 * Register Google fonts for Paloma FSE
+	 *
+	 * Create your own evit_fse_fonts_url() function to override in a child theme.
+	 *
+	 * @since 1.0
+	 *
+	 * @return string Google fonts URL for the theme.
+	 */
+	function evit_fse_fonts_url() {
+		$fonts_url = '';
+
+		/* Translators: If there are characters in your language that are not
+		* supported by Poppins, translate this to 'off'. Do not translate
+		* into your own language.
+		*/
+		$font_families = array( 'Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap', 'Heebo:wght@300;400;500;600;700;800;900&display=swap' );
+
+		if ( ! empty( $font_families  ) ) {
+
+			$query_args = array(
+				'family' => implode( '&family=', $font_families ), //urlencode( implode( '|', $font_families ) ),
+				// 'subset' => urlencode( 'latin,latin-ext' ),
+				'display' => 'swap',
+			);
+
+			$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css2' );
+		}
+
+		if ( ! class_exists( 'WPTT_WebFont_Loader' ) ) {
+			// Load Google fonts from Local.
+			require_once get_theme_file_path( 'inc/lib/wptt-webfont-loader.php' );
+		}
+
+		return esc_url( wptt_get_webfont_url( $fonts_url ) );
+	}
+endif;
