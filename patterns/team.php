9f26b25f9783e9b8a562ce2d6d612da27d58c2ba
Sabin2 <sabinbhurtel@gmail.com>
2023-01-09 15:52:25 +0545

    created team pattern

---
+++ patterns/team.php
@@ -0,0 +1,68 @@
+<?php
+ /**
+  * Title: Team
+  * Slug: evit-fse/team
+  * Categories: evit-fse
+  */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"0","right":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"87%"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:columns {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:0;margin-bottom:0"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|20"}}},"layout":{"type":"constrained","contentSize":"100%"}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"vivid-red","fontSize":"normal","fontFamily":"Kumbh Sans"} -->
+<p class="has-vivid-red-color has-text-color has-kumbh-sans-font-family has-normal-font-size" style="font-style:normal;font-weight:600">OUR TEAM</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"55px","fontStyle":"normal","fontWeight":"700"}}} -->
+<h2 style="font-size:55px;font-style:normal;font-weight:700">Meet our team</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}},"textColor":"body-text"} -->
+<p class="has-body-text-color has-text-color" style="line-height:1.6">Introducing our team of talented and skilled professionals who are ready to increase your productivity and bring your business to the new level of efficiency and sustainability.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"vivid-red","style":{"border":{"radius":"0px"}},"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-vivid-red-background-color has-background wp-element-button" style="border-radius:0px">Our Team</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"background","textColor":"foreground","style":{"border":{"radius":"0px"}},"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-foreground-color has-background-background-color has-text-color has-background wp-element-button" style="border-radius:0px"><strong>Video Presentation</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/assets/images/team1.jpg","id":583,"dimRatio":50,"contentPosition":"bottom center","isDark":false,"align":"center","style":{"color":{}}} -->
+<div class="wp-block-cover aligncenter is-light has-custom-content-position is-position-bottom-center"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-583" alt="" src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/team1.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group has-background-background-color has-background"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"19px"}}} -->
+<h4 style="font-size:19px">Ann Dowson</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"body-text"} -->
+<p class="has-body-text-color has-text-color">SEO Specialist</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/assets/images/team2.jpg","id":582,"dimRatio":50,"contentPosition":"bottom center","isDark":false,"align":"center","style":{"color":{}}} -->
+<div class="wp-block-cover aligncenter is-light has-custom-content-position is-position-bottom-center"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-582" alt="" src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/team2.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group has-background-background-color has-background"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"19px"}}} -->
+<h4 style="font-size:19px">Claire Jones</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"textTransform":"capitalize"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"textColor":"body-text"} -->
+<p class="has-body-text-color has-text-color" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;text-transform:capitalize">Creative Director</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
