5c2e5bc86897caae2265c806ef3e38c6fbcf7926
Sabin2 <sabinbhurtel@gmail.com>
2023-01-10 17:14:03 +0545

    designed contact page

---
+++ patterns/contact.php
@@ -0,0 +1,38 @@
+<?php
+ /**
+  * Title: Contact
+  * Slug: evit-fse/contact
+  * Categories: evit-fse
+  */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"var:preset|spacing|30","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"100%"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":618,"width":1188,"height":607,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/map.png" alt="" class="wp-image-618" width="1188" height="607"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="has-text-align-center">Contact Details</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|body-text"}}},"typography":{"letterSpacing":"1px","textDecoration":"none","textTransform":"none"}},"textColor":"body-text","fontSize":"normal","fontFamily":"poppins"} -->
+<p class="has-text-align-center has-body-text-color has-text-color has-link-color has-poppins-font-family has-normal-font-size" style="letter-spacing:1px;text-decoration:none;text-transform:none">Park Ln, Lalitpur 44600</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|body-text"}}},"typography":{"letterSpacing":"1px","textDecoration":"none","textTransform":"none"}},"textColor":"body-text","fontSize":"normal","fontFamily":"poppins"} -->
+<p class="has-text-align-center has-body-text-color has-text-color has-link-color has-poppins-font-family has-normal-font-size" style="letter-spacing:1px;text-decoration:none;text-transform:none">http://eaglevisionit.com/</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"typography":{"letterSpacing":"1px","textDecoration":"none","textTransform":"none","fontStyle":"normal","fontWeight":"700"}},"textColor":"foreground","fontSize":"normal","fontFamily":"poppins"} -->
+<p class="has-text-align-center has-foreground-color has-text-color has-link-color has-poppins-font-family has-normal-font-size" style="font-style:normal;font-weight:700;letter-spacing:1px;text-decoration:none;text-transform:none">9803315016</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"align":"center"} -->
+<ul class="wp-block-social-links aligncenter"><!-- wp:social-link {"url":"www.facebook.com/eaglevisionit","service":"facebook","label":""} /-->
+
+<!-- wp:social-link {"url":"www.twitter.com","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"www.instagram.com","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"www.wordpress.org","service":"wordpress"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
