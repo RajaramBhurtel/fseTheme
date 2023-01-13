d3eb2c34734ab0458f00b8fe649e2ef11dd38485
Sabin2 <sabinbhurtel@gmail.com>
2023-01-13 11:10:40 +0545

    designed service page

---
+++ patterns/service-page.php
@@ -0,0 +1,162 @@
+<?php
+ /**
+  * Title: Service Page
+  * Slug: evit-fse/service-page
+  * Categories: evit-fse
+  */
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30","margin":{"top":"var:preset|spacing|80"},"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained","contentSize":"87%"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--80);padding-right:0;padding-left:0"><!-- wp:heading {"textAlign":"center","level":1,"align":"full","style":{"typography":{"fontStyle":"normal","fontWeight":"800","fontSize":"50px"}},"textColor":"foreground"} -->
+<h1 class="alignfull has-text-align-center has-foreground-color has-text-color" style="font-size:50px;font-style:normal;font-weight:800">Services</h1>
+<!-- /wp:heading -->
+
+<!-- wp:image {"align":"center","id":650,"width":40,"height":52,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/down-arrow.png" alt="" class="wp-image-650" width="40" height="52"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"0","bottom":"var:preset|spacing|80","left":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"87%"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:columns {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0","left":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:0;margin-bottom:0"><!-- wp:column {"width":"40%","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:40%"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":816,"width":376,"height":250,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/wordpresst/wp-content/uploads/2023/01/services1.jpg" alt="" class="wp-image-816" width="376" height="250"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:pullquote {"style":{"color":{"background":"#074de3"},"border":{"radius":"0px","width":"0px","style":"none"}},"textColor":"background","className":"evit-service-textbox","fontSize":"normal"} -->
+<figure class="wp-block-pullquote evit-service-textbox has-background-color has-text-color has-background has-normal-font-size" style="border-style:none;border-width:0px;border-radius:0px;background-color:#074de3"><blockquote><p><strong>Endless Possibilities</strong></p><cite>Consectetur adipiscing elit, sed do eiusmod tempo.</cite></blockquote></figure>
+<!-- /wp:pullquote --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px"},"color":{"text":"#d90a2c"}},"fontFamily":"Kumbh Sans"} -->
+<p class="has-text-color has-kumbh-sans-font-family" style="color:#d90a2c;font-size:12px"><strong><strong>CORPORATE SERVICES</strong></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"50px","fontStyle":"normal","fontWeight":"700"}}} -->
+<h2 style="font-size:50px;font-style:normal;font-weight:700">We protect &amp; support your business.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"25%"} -->
+<div class="wp-block-column" style="flex-basis:25%"><!-- wp:spacer {"height":"162px"} -->
+<div style="height:162px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"75%"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:paragraph {"textColor":"body-text","fontSize":"normal"} -->
+<p class="has-body-text-color has-text-color has-normal-font-size">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"foreground","style":{"border":{"radius":"0px"},"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-outline","fontSize":"normal"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline has-normal-font-size" style="font-style:normal;font-weight:600"><a class="wp-block-button__link has-foreground-color has-text-color wp-element-button" href="http://localhost/wordpresst/about/" style="border-radius:0px">Read More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"backgroundColor":"body-text","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-body-text-color has-alpha-channel-opacity has-body-text-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:columns {"verticalAlignment":"center","align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"padding":{"right":"var:preset|spacing|80","left":"var:preset|spacing|80"}}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-center" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:column {"verticalAlignment":"center","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"}},"textColor":"vivid-red"} -->
+<p class="has-vivid-red-color has-text-color" style="font-size:13px"><strong><strong>CORPORATE SERVICES</strong></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"50px"}}} -->
+<h2 style="font-size:50px">Business strategies &amp; top performance.</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","textColor":"body-text","fontSize":"normal"} -->
+<div class="wp-block-column is-vertically-aligned-center has-body-text-color has-text-color has-normal-font-size"><!-- wp:spacer {"height":"167px"} -->
+<div style="height:167px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p>We appreciate your trust greatly! Our clients choose us and our products because they know we’re the best.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"ordered":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1.8"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|40"}}},"textColor":"black","fontSize":"big"} -->
+<ol class="has-black-color has-text-color has-big-font-size" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--40);font-style:normal;font-weight:600;line-height:1.8"><!-- wp:list-item -->
+<li>Endless Possibilities</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Working Strategies</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Process Optimization</li>
+<!-- /wp:list-item --></ol>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"},"border":{"width":"0px","style":"none"}},"layout":{"type":"constrained","justifyContent":"center","contentSize":"100%"}} -->
+<div class="wp-block-group" style="border-style:none;border-width:0px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|50"},"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0","bottom":"0"}},"border":{"width":"0px","style":"none","radius":"0px"}}} -->
+<div class="wp-block-columns" style="border-style:none;border-width:0px;border-radius:0px;margin-top:0;margin-bottom:0;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary1.jpg","id":502,"dimRatio":20,"overlayColor":"dark-blue","focalPoint":{"x":0.54,"y":0.55},"minHeight":405,"minHeightUnit":"px","contentPosition":"bottom center","isDark":false} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-bottom-center" style="min-height:405px"><span aria-hidden="true" class="wp-block-cover__background has-dark-blue-background-color has-background-dim-20 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-502" alt="" src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary1.jpg" style="object-position:54% 55%" data-object-fit="cover" data-object-position="54% 55%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"textColor":"background","fontSize":"large"} -->
+<h5 class="has-background-color has-text-color has-large-font-size">Market Research</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"color":{"text":"#f4eeee"}}} -->
+<p class="has-text-color" style="color:#f4eeee">Description about market research</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary3.jpg","id":501,"dimRatio":20,"overlayColor":"dark-blue","focalPoint":{"x":0.55,"y":0.5},"minHeight":405,"minHeightUnit":"px","contentPosition":"bottom center"} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="min-height:405px"><span aria-hidden="true" class="wp-block-cover__background has-dark-blue-background-color has-background-dim-20 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-501" alt="" src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary3.jpg" style="object-position:55% 50%" data-object-fit="cover" data-object-position="55% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"textColor":"background","fontSize":"large"} -->
+<h5 class="has-background-color has-text-color has-large-font-size">Style &amp; Identity</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"color":{"text":"#f4eeee"}}} -->
+<p class="has-text-color" style="color:#f4eeee">Our identity details</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary2.jpg","id":500,"dimRatio":20,"overlayColor":"black","focalPoint":{"x":0.39,"y":0.51},"minHeight":405,"minHeightUnit":"px","contentPosition":"bottom center"} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="min-height:405px"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim-20 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-500" alt="" src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary2.jpg" style="object-position:39% 51%" data-object-fit="cover" data-object-position="39% 51%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"textColor":"background","fontSize":"large"} -->
+<h5 class="has-background-color has-text-color has-large-font-size">Customer Support</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"color":{"text":"#f4eeee"}}} -->
+<p class="has-text-color" style="color:#f4eeee">How we support our customer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<div class="wp-block-buttons alignwide" style="margin-top:var(--wp--preset--spacing--70);margin-bottom:var(--wp--preset--spacing--70);font-style:normal;font-weight:600"><!-- wp:button {"style":{"border":{"radius":"0px"},"color":{"background":"#0727e3"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-background wp-element-button" href="http://localhost/wordpresst/about/" style="border-radius:0px;background-color:#0727e3">About Us</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
