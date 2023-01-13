4f51adb3babd8f5527aa8d66eee7aacfa067a554
Sabin2 <sabinbhurtel@gmail.com>
2023-01-13 14:44:44 +0545

    added contact form

--- patterns/contact.php
+++ patterns/contact.php
@@ -18,7 +18,9 @@
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.019954751195!2d85.3192630761938!3d27.685778126441143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19cdd0c2c6d1%3A0x5b2341291ef62a3a!2sEagle%20Vision%20It%20Pvt.%20Ltd.!5e0!3m2!1sen!2snp!4v1673502505937!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 <!-- /wp:html -->
 
-<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<!-- wp:columns {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60"},"blockGap":{"top":"var:preset|spacing|70","left":"var:preset|spacing|70"}}}} -->
+<div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--60)"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
 <h2 class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--40);margin-right:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);margin-left:var(--wp--preset--spacing--40)">Contact Details</h2>
 <!-- /wp:heading -->
 
@@ -32,10 +34,18 @@
 
 <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"typography":{"letterSpacing":"1px","textDecoration":"none","textTransform":"none","fontStyle":"normal","fontWeight":"700"}},"textColor":"foreground","fontSize":"normal","fontFamily":"poppins"} -->
 <p class="has-text-align-center has-foreground-color has-text-color has-link-color has-poppins-font-family has-normal-font-size" style="font-style:normal;font-weight:700;letter-spacing:1px;text-decoration:none;text-transform:none">9803315016</p>
-<!-- /wp:paragraph -->
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
 
-<!-- wp:social-links {"align":"center"} -->
-<ul class="wp-block-social-links aligncenter"><!-- wp:social-link {"url":"www.facebook.com/eaglevisionit","service":"facebook","label":""} /-->
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:shortcode -->
+[contact-form-7 id="829" title="Contact form 1"]
+<!-- /wp:shortcode --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:social-links {"align":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|50"}}}} -->
+<ul class="wp-block-social-links aligncenter" style="padding-top:var(--wp--preset--spacing--50)"><!-- wp:social-link {"url":"www.facebook.com/eaglevisionit","service":"facebook","label":""} /-->
 
 <!-- wp:social-link {"url":"www.twitter.com","service":"twitter"} /-->
 
