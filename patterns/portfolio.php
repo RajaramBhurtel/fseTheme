5817dcd49dea7445ffbc4fc54a91f39e99cef8fa
Sabin2 <sabinbhurtel@gmail.com>
2023-01-13 14:44:07 +0545

    added portfolio template

---
+++ patterns/portfolio.php
@@ -0,0 +1,50 @@
+<?php
+ /**
+  * Title: Portfolio
+  * Slug: evit-fse/portfolio
+  * Categories: evit-fse
+  */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50","margin":{"top":"var:preset|spacing|70"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"900"}},"textColor":"foreground","fontSize":"huge"} -->
+<h1 class="has-text-align-center has-foreground-color has-text-color has-huge-font-size" style="font-style:normal;font-weight:900">Portfolio</h1>
+<!-- /wp:heading -->
+
+<!-- wp:image {"align":"center","id":650,"width":40,"height":52,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/down-arrow.png" alt="" class="wp-image-650" width="40" height="52"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:gallery {"columns":3,"linkTo":"none","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|70","right":"0","bottom":"0","left":"0"}}}} -->
+<figure class="wp-block-gallery has-nested-images columns-3 is-cropped" style="margin-top:var(--wp--preset--spacing--70);margin-right:0;margin-bottom:0;margin-left:0"><!-- wp:image {"id":861,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/about.jpg" alt="" class="wp-image-861"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":856,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/port1.jpg" alt="" class="wp-image-856"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":857,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/banner.jpg" alt="" class="wp-image-857"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":859,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/ct2.jpg" alt="" class="wp-image-859"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":864,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary2.jpg" alt="" class="wp-image-864"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":366,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/port2.jpg" alt="" class="wp-image-366"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":858,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/gallary1.jpg" alt="" class="wp-image-858"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":865,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/port3.jpg" alt="" class="wp-image-865"/></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:group -->
