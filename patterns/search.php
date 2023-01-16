5624cdc0ed5cba35baacda5a397dbe267adc0627
Sabin2 <sabinbhurtel@gmail.com>
2023-01-16 17:19:30 +0545

    added search page

---
+++ patterns/search.php
@@ -0,0 +1,64 @@
+<?php
+ /**
+  * Title: Search
+  * Slug: evit-fse/search
+  * Categories: evit-fse
+  */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"background"} -->
+<div class="wp-block-group has-background-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:cover {"url":"http://localhost/wordpresst/wp-content/uploads/2023/01/table-structure-auditorium-ceiling-room-meeting-room-1262850-pxhere.com_.jpg","id":865,"dimRatio":40,"minHeight":538,"minHeightUnit":"px","isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"10vw","right":"30px","bottom":"10vw","left":"30px"}}}} -->
+<div class="wp-block-cover alignfull is-light" style="padding-top:10vw;padding-right:30px;padding-bottom:10vw;padding-left:30px;min-height:538px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-40 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-865" alt="" src="http://localhost/wordpresst/wp-content/uploads/2023/01/table-structure-auditorium-ceiling-room-meeting-room-1262850-pxhere.com_.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0px"}},"layout":{"inherit":false}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","textColor":"background"} -->
+<h2 class="has-text-align-center has-background-color has-text-color">Search Now </h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"50px"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-bottom:50px"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search Now","width":100,"widthUnit":"%","buttonText":"Search","align":"center","style":{"border":{"radius":"100px"},"color":{"background":"#144cda"}},"textColor":"background"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","right":"20px","bottom":"80px","left":"20px"},"blockGap":"0px"}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:80px;padding-right:20px;padding-bottom:80px;padding-left:20px"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"layout":{"type":"default"}} -->
+<div class="wp-block-column"><!-- wp:query {"queryId":39,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"layout":{"type":"default"}} -->
+<div class="wp-block-query"><!-- wp:post-template {"style":{"typography":{"textDecoration":"none"}}} -->
+<!-- wp:group {"style":{"typography":{"textDecoration":"none"}},"textColor":"body-text","layout":{"type":"constrained","contentSize":"87%"}} -->
+<div class="wp-block-group has-body-text-color has-text-color" style="text-decoration:none"><!-- wp:post-featured-image {"height":"400px"} /-->
+
+<!-- wp:post-title {"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"typography":{"textTransform":"none","textDecoration":"none"}},"textColor":"foreground"} /-->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:post-terms {"term":"category","style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}}},"textColor":"foreground","fontSize":"normal"} /-->
+
+<!-- wp:post-date {"textColor":"body-text"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:post-excerpt {"moreText":"Read more","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"}}}}} /-->
+
+<!-- wp:separator {"style":{"color":{"background":"#bac1c8"}},"className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#bac1c8;color:#bac1c8"/>
+<!-- /wp:separator --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:query-pagination {"paginationArrow":"arrow","align":"center","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<!-- wp:query-pagination-previous {"fontSize":"small"} /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next {"fontSize":"small"} /-->
+<!-- /wp:query-pagination -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+<p>No Posts Found</p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:query --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
