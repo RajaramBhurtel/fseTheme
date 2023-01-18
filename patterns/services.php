b9723c28a607c7413e255e4b267b9e49472ff282
Sabin2 <sabinbhurtel@gmail.com>
2023-01-18 11:34:47 +0545

    fixed styles

--- patterns/services.php
+++ patterns/services.php
@@ -1,16 +1,16 @@
 <?php
  /**
   * Title: Services
-  * Slug: columns/services
-  * Categories: columns
+  * Slug: evit-fse/services
+  * Categories: evit-fse
   */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"70px","right":"0px","bottom":"100px","left":"0px"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"87%"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:70px;padding-right:0px;padding-bottom:100px;padding-left:0px"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"50%","layout":{"type":"default"}} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap","justifyContent":"left"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","letterSpacing":"2px","textDecoration":"none","lineHeight":"1.5"},"color":{"text":"#d90a2c"}},"fontFamily":"Kumbh Sans"} -->
-<p class="has-text-color has-kumbh-sans-font-family" style="color:#d90a2c;font-size:12px;letter-spacing:2px;line-height:1.5;text-decoration:none;text-transform:uppercase"><strong>WHAT WE DO</strong></p>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"100px","right":"0px","bottom":"100px","left":"0px"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"87%"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:100px;padding-right:0px;padding-bottom:100px;padding-left:0px"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"50%","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","letterSpacing":"2px","textDecoration":"none","lineHeight":"1.5"}},"textColor":"primary","fontFamily":"Kumbh Sans"} -->
+<p class="has-primary-color has-text-color has-kumbh-sans-font-family" style="font-size:12px;letter-spacing:2px;line-height:1.5;text-decoration:none;text-transform:uppercase"><strong>WHAT WE DO</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"style":{"typography":{"fontSize":"50px","fontStyle":"normal","fontWeight":"700"}},"fontFamily":"Kumbh Sans"} -->
@@ -26,9 +26,9 @@
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
-<!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"50%","style":{"spacing":{"blockGap":"45px"}}} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<!-- wp:columns {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"width":"50%","style":{"spacing":{"blockGap":"45px","padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:50%"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
 <div class="wp-block-columns" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"verticalAlignment":"center","width":"100%","style":{"spacing":{"blockGap":"4px"}}} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%"><!-- wp:paragraph {"textColor":"body-text","fontSize":"normal","fontFamily":"Kumbh Sans"} -->
 <p class="has-body-text-color has-text-color has-kumbh-sans-font-family has-normal-font-size">We do our best to implement your ideas into the project to make it successful and profitable. The choices you make define your achievements.</p>
@@ -56,7 +56,7 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"var:preset|spacing|20"}}}} -->
-<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"align":"left","dropCap":true,"textColor":"body-text","fontSize":"normal","fontFamily":"Kumbh Sans"} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0)"><!-- wp:paragraph {"align":"left","dropCap":true,"textColor":"body-text","fontSize":"normal","fontFamily":"Kumbh Sans"} -->
 <p class="has-drop-cap has-text-align-left has-body-text-color has-text-color has-kumbh-sans-font-family has-normal-font-size">Set new business priorities, outline plans for growth, determine a product roadmap or plan your decisions – we can handle it.</p>
 <!-- /wp:paragraph -->
 
@@ -65,8 +65,8 @@
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:paragraph {"textColor":"body-text","fontSize":"normal","fontFamily":"Kumbh Sans"} -->
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph {"textColor":"body-text","fontSize":"normal","fontFamily":"Kumbh Sans"} -->
 <p class="has-body-text-color has-text-color has-kumbh-sans-font-family has-normal-font-size">Digitalizing trends and disruptive technologies are giving companies the ability to transform their business models.</p>
 <!-- /wp:paragraph -->
 
