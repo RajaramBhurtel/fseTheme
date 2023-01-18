dc3fc16c32dae7b9158c5f42c5f4ddb42f0301c5
Sabin2 <sabinbhurtel@gmail.com>
2023-01-18 11:40:29 +0545

    fixed padding

--- patterns/creative.php
+++ patterns/creative.php
@@ -5,10 +5,10 @@
   * Categories: evit-fse
   */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|40","bottom":"var:preset|spacing|80","left":"var:preset|spacing|50"},"blockGap":"0","margin":{"top":"0","bottom":"0"}},"color":{"background":"#f5f5f5"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group has-background" style="background-color:#f5f5f5;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--50)"><!-- wp:columns {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<div class="wp-block-columns" style="margin-top:0;margin-bottom:0"><!-- wp:column {"width":""} -->
-<div class="wp-block-column"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}},"color":{"background":"#f5f5f5"}},"layout":{"type":"constrained","contentSize":"87%"}} -->
+<div class="wp-block-group has-background" style="background-color:#f5f5f5;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"padding":{"top":"100px","bottom":"100px"}}}} -->
+<div class="wp-block-columns" style="padding-top:100px;padding-bottom:100px"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
 <div class="wp-block-columns are-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":569,"sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/ct1.jpg" alt="" class="wp-image-569"/></figure>
@@ -23,10 +23,10 @@
 <!-- /wp:columns --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|70"}}}} -->
-<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--70)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px"},"color":{"text":"#d90a2c"}},"fontFamily":"Kumbh Sans"} -->
-<p class="has-text-color has-kumbh-sans-font-family" style="color:#d90a2c;font-size:12px"><strong>CREATIVE APPROACH</strong></p>
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"right":"0","bottom":"0px","left":"var:preset|spacing|70","top":"0px"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0px;padding-right:0;padding-bottom:0px;padding-left:var(--wp--preset--spacing--70)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px"}},"textColor":"primary","fontFamily":"Kumbh Sans"} -->
+<p class="has-primary-color has-text-color has-kumbh-sans-font-family" style="font-size:12px"><strong>CREATIVE APPROACH</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"style":{"typography":{"fontSize":"50px","fontStyle":"normal","fontWeight":"700"}}} -->
@@ -37,8 +37,8 @@
 <p class="has-body-text-color has-text-color has-normal-font-size">For those who love videos, animation and motion graphics, we have come up with a new cool project!</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:list {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","right":"0","bottom":"0","left":"0"}},"typography":{"lineHeight":"1.9"}},"textColor":"body-text","fontSize":"normal"} -->
-<ul class="has-body-text-color has-text-color has-normal-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;line-height:1.9"><!-- wp:list-item -->
+<!-- wp:list {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"var:preset|spacing|40"},"margin":{"top":"0","right":"0","bottom":"0","left":"0"}},"typography":{"lineHeight":"1.9"}},"textColor":"body-text","fontSize":"normal"} -->
+<ul class="has-body-text-color has-text-color has-normal-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--40);line-height:1.9"><!-- wp:list-item -->
 <li>Amazing photo &amp; video galleries</li>
 <!-- /wp:list-item -->
 
@@ -52,8 +52,8 @@
 <!-- /wp:list -->
 
 <!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":"0px"},"color":{"background":"#0840e8"}},"className":"is-style-fill","fontSize":"normal"} -->
-<div class="wp-block-button has-custom-font-size is-style-fill has-normal-font-size"><a class="wp-block-button__link has-background wp-element-button" href="#" style="border-radius:0px;background-color:#0840e8">Services</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"primary","textColor":"background","style":{"border":{"radius":"0px"},"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill" style="font-style:normal;font-weight:600"><a class="wp-block-button__link has-background-color has-primary-background-color has-text-color has-background wp-element-button" href="http://localhost/wordpresst/services/" style="border-radius:0px">Services</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
